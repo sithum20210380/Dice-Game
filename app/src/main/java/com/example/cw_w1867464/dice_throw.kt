acc5afaa707e50042ed914fee4675b0dba13482e
Sithum Raveesha <85311920+sithum20210380@users.noreply.github.com>
2023-03-19 19:34:11 +0530

    completed

--- app/src/main/java/com/example/cw_w1867464/dice_throw.kt
+++ app/src/main/java/com/example/cw_w1867464/dice_throw.kt
@@ -1,7 +1,9 @@
 package com.example.cw_w1867464
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -23,7 +25,9 @@ class ThirdActivity : AppCompatActivity() {
     private lateinit var playerScoreView: TextView
     private lateinit var computerScoreView: TextView
     private lateinit var scoreCounter: TextView
-    //private lateinit var rollButton: Button
+    private lateinit var humanPlayer : TextView
+    private lateinit var computerPlayer : TextView
+    private lateinit var instructionButton: Button
 
     private var playerScore = 0
     private var computerScore = 0
@@ -35,9 +39,11 @@ class ThirdActivity : AppCompatActivity() {
 
     private var selectedDice: Int? = null
 
-    // initialize wins count for human and computer
-    private var humanWins = 0
-    private var computerWins = 0
+    // initialize wins count for human and computer to remain even it navigates to another page
+    companion object{
+        private var humanWins = 0
+        private var computerWins = 0
+    }
 
 
     @SuppressLint("MissingInflatedId")
@@ -48,11 +54,14 @@ class ThirdActivity : AppCompatActivity() {
         throwButton = findViewById(R.id.throw_button)
         scoreButton = findViewById(R.id.score_button)
         resetButton = findViewById(R.id.reset_button)
+        instructionButton =findViewById(R.id.instruction_button)
 
         playerScoreView = findViewById(R.id.player_score)
         computerScoreView = findViewById(R.id.computer_score)
         targetScoreInput = findViewById(R.id.target_score_input)
         scoreCounter = findViewById(R.id.wins_view)
+        humanPlayer = findViewById(R.id.human_player)
+        computerPlayer = findViewById(R.id.computer_player)
 
         humanDiceViews = listOf(
             findViewById(R.id.human_dice1),
@@ -70,18 +79,20 @@ class ThirdActivity : AppCompatActivity() {
             findViewById(R.id.computer_dice5)
         )
         // display the initial score of human and computer players
-        scoreCounter.text = "H:0/C:0"
+        scoreCounter.text = "H:$humanWins/C:$computerWins"
 
-        fun calculateScoreFromImages(diceValues: List<Int>): Int {
+        //method to calculate score from dice images
+        fun calculateScoreFromImages(diceValues: List<Int> , selectedDice: Int?): Int {
             var score = 0
             var hasStraight = true
             var counts = IntArray(6)
 
             for (i in diceValues.indices) {
+                if (i != selectedDice){
                 val diceValue = diceValues[i]
                 counts[diceValue - 1]++
                 score += diceValue
-            }
+            }}
 
             for (i in counts.indices) {
                 if (counts[i] != 1) {
@@ -100,6 +111,7 @@ class ThirdActivity : AppCompatActivity() {
         val winColor = Color.GREEN
         val loseColor = Color.RED
 
+        // click listener for the throw button
         throwButton.setOnClickListener {
             val customScore = targetScoreInput.text.toString().toIntOrNull()
             if (customScore != null) {
@@ -114,16 +126,18 @@ class ThirdActivity : AppCompatActivity() {
             // Display human dice images
             displayDice(computerDiceViews, computerThrows)
             // Calculate human score based on the dice image
+            var humanScoreFromImages = calculateScoreFromImages(humanThrows,selectedDice)
+            var computerScoreFromImages = calculateScoreFromImages(computerThrows,null)
+            playerScore += humanScoreFromImages
+            computerScore += computerScoreFromImages
+            humanPlayer.text = "Player: $playerScore"
+            computerPlayer.text = "Computer: $computerScore"
 
             throwCounter++
             throwButton.text = "Reroll ($throwCounter/3)"
             if (throwCounter >= 3) {
-                var humanScoreFromImages = calculateScoreFromImages(humanThrows)
-                var computerScoreFromImages = calculateScoreFromImages(computerThrows)
-                playerScore += humanScoreFromImages
-                computerScore += computerScoreFromImages
-                playerScoreView.text = "Player Score: $playerScore"
-                computerScoreView.text = "Computer Score: $computerScore"
+                playerScoreView.text = "Player: $playerScore"
+                computerScoreView.text = "Computer: $computerScore"
                 if (playerScore >= targetScore && playerScore >= computerScore) {
                     endGame("Player", winColor)
                     throwButton.isEnabled = false
@@ -137,21 +151,40 @@ class ThirdActivity : AppCompatActivity() {
 
         }
 
+        // click listener for the reset button
         resetButton.setOnClickListener {
             resetGame()
         }
 
+        // click listener for the instruction button
+        instructionButton.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.rules,null)
+
+            val myDialognew = Dialog(this)
+            myDialognew.setContentView(dialogBinding)
+
+            myDialognew.setCancelable(true)
+            myDialognew.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialognew.show()
+
+            val closebtn = dialogBinding.findViewById<TextView>(R.id.close)
+            closebtn.setOnClickListener{
+                myDialognew.dismiss()
+            }
+        }
+
+        // click listener for the score button
         scoreButton.setOnClickListener {
             // Calculate computer's score
             val computerThrows = rollDice1()
-            var computerScoreFromImages = calculateScoreFromImages(computerThrows)
+            var computerScoreFromImages = calculateScoreFromImages(computerThrows,null)
             computerScore += computerScoreFromImages
-            computerScoreView.text = "Computer Score: $computerScore"
+            computerScoreView.text = "Computer: $computerScore"
 
             val humanThrows = rollDice1()
-            var humanScoreFromImages = calculateScoreFromImages(humanThrows)
+            var humanScoreFromImages = calculateScoreFromImages(humanThrows,selectedDice)
             playerScore += humanScoreFromImages
-            playerScoreView.text = "Player Score: $playerScore"
+            playerScoreView.text = "Player: $playerScore"
 
             if (playerScore >= targetScore && playerScore >= computerScore) {
                 endGame("Player", winColor)
@@ -163,7 +196,7 @@ class ThirdActivity : AppCompatActivity() {
         }
     }
 
-
+    // function that olls five dice an returns the sum of their face values
     private fun rollDice(): Int {
         var sum = 0
         for (i in 1..5) {
@@ -173,19 +206,21 @@ class ThirdActivity : AppCompatActivity() {
         return sum
     }
 
+    // function to reset the values
     private fun resetGame() {
         playerScore = 0
         computerScore = 0
-        playerScoreView.text = "Player Score: 0"
-        computerScoreView.text = "Computer Score: 0"
+        playerScoreView.text = "Player: 0"
+        computerScoreView.text = "Computer: 0"
         //winnerView.text = ""
         throwButton.text = "Reroll (1/3)"
         throwButton.isEnabled = true
+        scoreButton.isEnabled =true
         displayDice(humanDiceViews, listOf(1, 1, 1, 1, 1))
         displayDice(computerDiceViews, listOf(1, 1, 1, 1, 1))
     }
 
-
+    //function to displays an alert dialog to inform the user whether they won or lost the game.
     fun endGame(winner: String, color: Int) {
         val message = if (winner == "Player") "You win!" else "You lose"
         val alertDialog = AlertDialog.Builder(this)
@@ -198,7 +233,7 @@ class ThirdActivity : AppCompatActivity() {
         alertDialog.setOnShowListener {
             alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(color)
         }
-
+        // calculate total wins of computer and user.
         if (winner == "Player") {
             humanWins++
         } else {
@@ -209,13 +244,20 @@ class ThirdActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
-
+    //  function to generate a list of 5 random integers between 1 and 6 (inclusive) representing
+    //  the faces of 5 dice being rolled.
     private fun rollDice1(): List<Int> {
         return List(5) { Random.nextInt(1, 7) }
     }
 
+
     private fun displayDice(diceViews: List<ImageView>, throws: List<Int>) {
         for (i in 0 until 5) {
+            if (selectedDice != null && selectedDice == i) {
+                // If the user selected a dice, skip rolling it
+                continue
+            }
+
             val drawableResource = when (throws[i]) {
                 1 -> R.drawable.dice_1
                 2 -> R.drawable.dice_2
@@ -225,7 +267,6 @@ class ThirdActivity : AppCompatActivity() {
                 else -> R.drawable.dice_6
             }
             diceViews[i].setImageResource(drawableResource)
-            //diceViews[i].tag = throws[i]
             diceViews[i].setOnClickListener {
                 if (selectedDice == i) {
                     // Deselect the dice if it's already selected
